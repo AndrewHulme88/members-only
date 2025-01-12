db8df92c21ca882a0d68d32abb8440e9920208db
andrew hulme <andrew_hulme04@hotmail.com>
2025-01-12 14:30:29 +1100

    Created user route and transferred sign up functionality from app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+const userRouter = require("./routes/user");
 const PORT = 3000;
 
 const pool = new Pool({ connectionString: "postgresql:andrew:mydb@localhost:5432/members_only_db" });
@@ -16,20 +17,8 @@ app.use(session({ secret: "cats", resave: false, saveUninitialized: false}));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
+app.use("/users", userRouter);
 
-app.post("/sign-up", async (req, res, next) => {
-  try {
-    await pool.query("INSERT INTO users (firstName, lastName, email, password) VALUES ($1, $2, $3, $4)", [
-      req.body.firstName,
-      req.body.lastName,
-      req.body.email,
-      req.body.password,
-    ]);
-    res.redirect("/");
-  } catch (err) {
-    return next(err);
-  }
-});
+app.get("/", (req, res) => res.render("index"));
 
 app.listen(PORT, () => console.log("App listening on port 3000"));
