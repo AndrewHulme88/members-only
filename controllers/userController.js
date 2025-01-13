5d45b51dd6ed5277480c5b508bc099710cc74511
andrew hulme <andrew_hulme04@hotmail.com>
2025-01-13 19:54:43 +1100

    Added confirm password, but error messages not showing

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const { Pool } = require("pg");
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcryptjs");
+const { body, validationResult } = require("express-validator");
 
 passport.use(
   new LocalStrategy(
@@ -53,23 +54,51 @@ module.exports = {
   },
 
   signUpForm: (req, res) => {
-    res.render("sign-up-form");
+    res.render("sign-up-form", {
+      user: { firstName: "", lastName: "", email: ""},
+      errors: []
+    });
   },
 
-  signUp: async (req, res, next) => {
-    const { firstName, lastName, email, password } = req.body;
-    try {
-      const hashedPassword = await bcrypt.hash(password, 10);
-      const result = await pool.query(
-        "INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)",
-        [firstName, lastName, email, hashedPassword]
-      );
-      res.redirect("/users/login");
-    } catch (err) {
-      console.error("Error adding user", err);
-      res.status(500).json({ error: "Failed to add user" });
-    }
-  },
+  signUp: [
+    body("firstName").notEmpty().withMessage("First name is required"),
+    body("lastName").notEmpty().withMessage("Last name is required"),
+    body("email").isEmail().normalizeEmail().withMessage("Please provide a valid email address"),
+    body("password").isLength({ min:6 }).withMessage("Password must be at least 6 characters").matches(/\d/).withMessage("Password must contain a number"),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Password confirmation does not match password");
+      }
+      return true;
+    }),
+
+    async (req, res, next) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.render("sign-up-form", {
+          user: {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email
+          },
+          errors: errors.array()
+        });
+      }
+
+      const { firstName, lastName, email, password } = req.body;
+      try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await pool.query(
+          "INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)",
+          [firstName, lastName, email, hashedPassword]
+        );
+        res.redirect("/users/login");
+      } catch (err) {
+        console.error("Error adding user", err);
+        res.status(500).json({ error: "Failed to add user" });
+      }
+    },
+  ],
 
   loginForm: (req, res) => {
     res.render("login");
