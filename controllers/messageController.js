0a951ff39ed1be875c5040f55a299f94d851e65a
andrew hulme <andrew_hulme04@hotmail.com>
2025-01-13 22:49:13 +1100

    Implemented create message functionality

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -9,4 +9,33 @@ module.exports = {
     }
     res.render("new-message", { errors: [], title: "", text: "" });
   },
-}
+
+  createMessage: [
+    body("title").trim().notEmpty().withMessage("Title is required"),
+    body("text").trim().notEmpty().withMessage("Message text required"),
+
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.render("new-message", {
+          errors: errors.array(),
+          title: req.body.title,
+          text: req.body.text
+        });
+      }
+
+      try {
+        const { title, text } = req.body;
+        const userId = req.user.id;
+        await pool.query(
+          "INSERT INTO messages (user_id, title, text, created_at) VALUES ($1, $2, $3, NOW())",
+          [userId, title, text]
+        );
+        res.redirect("/users");
+      } catch (err) {
+        console.error("Error creating message", err);
+        res.status(500).send("Error creating message");
+      }
+    }
+  ]
+};
--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -3,5 +3,6 @@ const messageRouter = express.Router();
 const messageController = require("../controllers/messageController");
 
 messageRouter.get("/new", messageController.newMessage);
+messageRouter.post("/new", messageController.createMessage);
 
 module.exports = messageRouter;
