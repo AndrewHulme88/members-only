5f8bc2a1d195dcdf3067e36d83cf499ed896b996
andrew hulme <andrew_hulme04@hotmail.com>
2025-01-14 21:13:42 +1100

    Made it so that the name of the message author shows up when logged in as member

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,14 +4,24 @@ const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 module.exports = {
   showHomePage: async (req, res) => {
     try {
-      if (req.isAuthenticated()) {
-        const { rows: messages } = await pool.query("SELECT * FROM messages ORDER BY created_at DESC");
-        return res.render("index", { user: req.user, messages });
-      }
-      res.render("index", { user: null, messages: [] });
+      let messagesQuery = `
+      SELECT messages.title, messages.text, messages.created_at, users.first_name, users.last_name
+      FROM messages
+      JOIN users ON messages.user_id = users.id
+      ORDER BY messages.created_at DESC
+    `;
+    const { rows: messages } = await pool.query(messagesQuery);
+
+      res.render("index", { user: req.user, messages });
     } catch (err) {
       console.error("Error fetching messages", err);
       res.status(500).send("An error occured while loading messages.");
     }
   }
 };
+
+
+// if (req.isAuthenticated()) {
+//   const { rows: messages } = await pool.query("SELECT * FROM messages ORDER BY created_at DESC");
+//   return res.render("index", { user: req.user, messages });
+// }
